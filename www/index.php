7bb901983e651c6833883c40ba066e230abf845d
nicolesteffens0 <156947143+nicolesteffens0@users.noreply.github.com>
2024-04-13 10:23:42 -0700

    Update index.php
    Simplified initial website page by getting rid of the other things we don't use

--- www/index.php
+++ www/index.php
@@ -25,10 +25,9 @@ ini_set('display_errors', 1);
 		<br />
     <div class="row marketing">
         <div style="border:1px solid #000000; padding: 10px">
-          <b>You can use the following users or register your own for the various exercises in the application.</b><br />
+          <b>Use the username and password combo below.</b><br />
         <ul>
           <li>bob:password</li>
-          <li>voldemort:horcrux</li>
         </ul>
         </div>
         <br />
@@ -45,30 +44,10 @@ ini_set('display_errors', 1);
         <div class="col-lg-6">
 
     <div style="border:1px solid #000000; padding: 10px">
-		  <h4><a href="register.php" style="color:#B31D14">register.php - user registration page</a></h4>
-          <p>This page can be used to create users that will be used throughout the application.</p>
-		
-          <h4><a href="login1.php" style="color:#B31D14">login1.php - basic injection page</a></h4>
-          <p>This page contains the most simplistic form of SQL injection flaw. Verbose errors, can be used to enumerate columns and bypass user authentication altogether</p>
-
-		  <h4><a href="login2.php" style="color:#B31D14">login2.php - basic injection page with brackets</a></h4>
-          <p>This page contains the most simplistic form of SQL injection flaw. Verbose errors, can be used to enumerate columns and bypass user authentication altogether. Backend query uses brackets to enclose variables. Very common on the Internet.</p>
-		  
-          <h4><a href="searchproducts.php" style="color:#B31D14">searchproducts.php - multiple exercises</a></h4>
-          <p>Page contains code that fetches multiple entries from the DB, can be abused to extract arbitrary data</p>
-
-          <h4><a href="secondorder_register.php" style="color:#B31D14">secondorder_register.php - allows registration with quotes</a></h4>
-          <p>Page allows user registration even with quotes. Quotes are nullified by appending a second quote to make them literals. Data is stored to backend tables without being verified if data was clean or not. The problem with doubling-up approach arises in more complex situations where the same item of data passes through several SQL queries, being written to the database and then read back more than once.</p>
-		  		  
-		  <h4><a href="secondorder_changepass.php" style="color:#B31D14">secondorder_changepass.php - allows users to change their password</a></h4>
-          <p>Retrieves the username from the database as is and uses it to construct a new query without sanitising. An example of how data coming from backend databases is also input to an application. Generates SQL errors if the data stored to the DB is unclean (using Second Order Register for example).</p>
-		  
-		  <h4><a href="blindsqli.php?user=<?php echo $_SESSION['username'];?>"  style="color:#B31D14">blindsqli.php - vulnerable to content and time based blind SQLi</a></h4>
-          <p>Page is vulnerable to blind sql injection using both changes in content as well as response times. Data can be extracted using true and false statements.</p>
-		  
+		 
+          <p>Click on this link to go to the vulnerable login page.</p>	  
 		  <h4><a href="os_sqli.php?user=<?php echo $_SESSION['username'];?>" style="color:#B31D14">os_sqli.php - can be used to interact with the filesystem and the OS via the MySQL databases</a></h4>
-          <p>Can be used to interact with the OS, including reading and writing of files and other tasks.</p>
-        </div>
+          </div>
       </div>
 
 </div>
